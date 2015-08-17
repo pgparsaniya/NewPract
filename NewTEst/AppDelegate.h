c3952ba498a3a6320e245d7f3cb803a02baef10f
pgparsaniya <piyush.parsaniya0125@gmail.com>
2015-08-17 14:10:24 +0530

    Initial Commit

---
+++ NewTEst/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewTEst
+//
+//  Created by Shine Technolab on 17/08/15.
+//  Copyright (c) 2015 Shine Technolab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewTEst/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewTEst
+//
+//  Created by Shine Technolab on 17/08/15.
+//  Copyright (c) 2015 Shine Technolab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
